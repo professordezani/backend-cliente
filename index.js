facc258ba68c88b6b7fe425395e14176fbcd3ca4
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2020-02-19 15:36:13 +0000

    Versão da aula 19/02

--- index.js
+++ index.js
@@ -20,4 +20,16 @@ server.get('/cliente', function(request, response) {
     return response.json(clientes);
 });
 
+server.get('/cliente/:id', function(request, response) { 
+    const id = request.params.id;
+    const cliente = clientes.filter(c => c.id == id);
+    return response.json(cliente);
+});
+
+server.delete('/cliente/:id', function(request, response) { 
+    const id = request.params.id;
+    clientes = clientes.filter(c => c.id != id);
+    return response.status(200).send();
+})
+
 server.listen(3000);
