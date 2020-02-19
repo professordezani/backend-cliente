6a320381114ecc8c1f7eded8ad51f6bdb7d48df3
Prof. Dr. Henrique Dezani <dezani@fatecriopreto.edu.br>
2020-02-19 14:37:29 +0000

    Versão inicial

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+// adicionar a biblioteca express:
+const express = require('express');
 
+// base de dados volátil e estática:
+var clientes = [
+    {
+        id: 1,
+        nome: 'Fulano',
+        email: 'fulano@email.com'
+    },
+    {
+        id: 2,
+        nome: 'Ciclano'
+    }
+];
+
+const server = express();
+
+server.get('/cliente', function(request, response) { 
+    return response.json(clientes);
+});
+
+server.listen(3000);
